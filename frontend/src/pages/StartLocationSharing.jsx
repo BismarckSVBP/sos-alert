bd4a63052a20d8161f17b803a2cc96e15124b220
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-07-13 01:59:35 +0530

    Update StartLocationSharing.jsx

--- frontend/src/pages/StartLocationSharing.jsx
+++ frontend/src/pages/StartLocationSharing.jsx
@@ -367,7 +367,7 @@ const StartLocationSharing = () => {
           hasStartedSharingRef.current = true;
           setIsSharing(true);
           setViewers(Math.floor(Math.random() * 3) + 1);
-          toast({ title: "Live l sharing started." });
+          toast({ title: "Live location sharing started." });
         }
 
         startWatchLocation();
