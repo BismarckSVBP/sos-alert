6fd25dc520c82764c691aece9dc4b0b6efbd5fdb
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-07-13 01:45:22 +0530

    Update socket.js

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,18 +1,1 @@
 
-import { io } from "socket.io-client";
-
-import { API_BASE_URL } from "../config";
-
-const socket = io(API_BASE_URL, {
-  transports: ["websocket"],
-  withCredentials: true,
-  autoConnect: false, // manual control
-});
-
-
-// const socket = io(`${API_BASE_URL}`, {
-//   withCredentials: true,
-// });
-
-export default socket;
-
